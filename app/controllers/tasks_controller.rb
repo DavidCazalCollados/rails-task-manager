2e16525e32d675f70a3169c2c7027a6e0825c4a4
David Cazal <da.cazal95@gmail.com>
2024-11-19 16:55:40 -0500

    CHallenge completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
     @taskstate = ""
     if @task.completed
       @taskstate = "completed"
@@ -24,18 +25,15 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params_update)
 
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path, status: :see_other
   end
@@ -48,5 +46,9 @@ class TasksController < ApplicationController
 
   def task_params_update
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
